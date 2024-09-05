57310140d95bcec46f73476dab0c606bc0f4a244
Egor Polyakov <leftgoga@yandex.ru>
2024-09-06 04:11:33 +0700

    Добавил раг пайплайн, сделанный вплоть до ретривера(пока неуверен насчет качества ретривинга)

--- .venv/DB/db_connector.py
+++ .venv/DB/db_connector.py
@@ -1,6 +1,7 @@
 import chromadb as cdb
 import os.path
-
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 class db_connector:
         '''
               Базовый коннектор векторки, создание самой бд, коллекций, запросы по добавлению, удалению, текстовый запрос
@@ -11,7 +12,7 @@ class db_connector:
                 self.client = None
                 self.coll = None
                 self.coll_list = []
-                self.embed_fun = None
+                self.embed_fun = HuggingFaceEmbeddings(model_name = "cointegrated/rubert-tiny2")
         def create_db(self,db_path):
 
                 self.client = cdb.PersistentClient(path = db_path,settings = cdb.config.Settings(allow_reset=True)
@@ -52,3 +53,17 @@ class db_connector:
 
         def reset_db(self):
                 self.client.reset()
+        def as_vector(self):
+                if self.embed_fun:
+                        vector = Chroma(
+                                client=self.client,
+                                collection_name="docs",
+                                embedding_function= self.embed_fun
+                        )
+                else:
+                        vector = Chroma(
+                                client=self.client,
+                                collection_name="docs"
+                        )
+
+                return vector
