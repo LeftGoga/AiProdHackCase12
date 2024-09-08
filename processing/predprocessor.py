5b7ea3a157b8b04172b03a605541bdfb1dfcb681
dmitry(danitaso) <dimaredko444@gmail.com>
2024-09-08 09:04:12 +0300

    Сделал черновой вариант предпроцессора пдф, ждем ламму и тесеракт в облаке

--- processing/predprocessor.py
+++ processing/predprocessor.py
@@ -3,6 +3,7 @@ from processing.pdf_predprocessor import PDFProcessor
 from processing.tables_predprocessor import PDFTableExtractor
 import os
 import json
+import PyPDF2
 
 class Preprocessor:
     def __init__(self):
@@ -16,13 +17,23 @@ class Preprocessor:
         if extension in ['.doc', '.docx']:
             return self.doc_parser.parse(file_path)
         elif extension == '.pdf':
+
+            page_count = self.get_pdf_page_count(file_path)
+            # for page in range(page_count):
             pdf_parser = PDFProcessor()
-            pdf_parser.process_pdf_page(file_path, 12)
-            
+            text = pdf_parser.process_pdf_page(file_path, 12)
+            print(text)
             table_extractor = PDFTableExtractor()  
-            table_extractor.extract_tables_on_page()
-            print(json.dumps(page_data, indent=4, ensure_ascii=False))
+            tables_list = table_extractor.extract_tables_from_pdf(file_path, 12, combine_tables=True)
+            """
+            Тут вызов ламмы для самари таблицы
+            """
+            print(tables_list)
 
         else:
             raise ValueError(f"Unsupported file extension: {extension}")
         
+    def get_pdf_page_count(self, file_path):
+        with open(file_path, 'rb') as file:
+            reader = PyPDF2.PdfReader(file)
+            return len(reader.pages)
