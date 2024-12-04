d7710ee8de9c1f7874b4efabbd761dc3975fd5da
Egor Polyakov <leftgoga@yandex.ru>
2024-12-04 11:03:27 +0700

    убрал айдишнки

--- models/llm_translator.py
+++ models/llm_translator.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 class YandexTranslate:
-    def __init__(self, api_key= 'AQVN3nS4abMnLQnzj0cx7iI-WF-6LFXHVzrU6y6S', api_id = 'ajeueis6jcp75k4v73q7', folder_id = 'b1gjj01i0f670l2o8hrs', target_language='ru'):
+    def __init__(self, api_key= '<Translator_key>', api_id = '<>api_id>', folder_id = 'folder_id', target_language='ru'):
         self.api_key = api_key
         self.api_id = api_id
         self.folder_id = folder_id
